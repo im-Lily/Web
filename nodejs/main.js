a0145a8f4f3114933dcafc7de528d790075bac8d
im-Lily <dmsruddlvhs2013@gmail.com>
2021-07-05 16:51:53 +0900

    query string url need to review

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,17 +1,21 @@
+// url을 통해 입력된 값 사용방법
+
 var http = require('http');
 var fs = require('fs');
+var url = require('url'); // url : node.js 빌트인 모듈
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id); // 객체 형태로 반환
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 
 });
 app.listen(3000);
