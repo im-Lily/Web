882c76bb88e732232ea57142c00e36448dd0a85b
im-Lily <dmsruddlvhs2013@gmail.com>
2022-03-09 19:30:18 +0900

    feat: add jquery library example

--- web1/colors.js
+++ web1/colors.js
@@ -1,27 +1,21 @@
 var Body = {
     // setColor 프로퍼티 추가
     setColor: function (color) {
-        document
-            .querySelector('body')
-            .style
-            .color = color;
+        $('body').css('color', color);
+        // document     .querySelector('body')     .style     .color = color;
     },
     setBackgroudColor: function (color) {
-        document
-            .querySelector('body')
-            .style
-            .backgroundColor = color;
+        $('body').css('backgroudColor', color);
+        // document     .querySelector('body')     .style     .backgroundColor = color;
     }
 }
 
 var Links = {
     setColor: function (color) {
-        var alist = document.querySelectorAll('a')
-        var i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i = i + 1;
-        }
+        // 모든 a 태그를 jquery로 제어
+        $('a').css('color', color); // jquery 사용
+        // var alist = document.querySelectorAll('a') var i = 0; while (i <
+        // alist.length) {     alist[i].style.color = color;     i = i + 1; }
     }
 }
 
